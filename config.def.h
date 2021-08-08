08ac054410d9111221d758a02c03e6692e27f35b
nugrhrizki <nugraha07rizki@gmail.com>
2021-08-09 00:39:09 +0700

    patch: underline tags

--- config.def.h
+++ config.def.h
@@ -32,6 +32,11 @@ static char *colors[][3] = {
 /* tagging */
 static const char *tags[] = { "", " ", " ", " ", " " };
 
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 3;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
