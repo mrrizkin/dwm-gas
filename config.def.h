653c29c92380c8ac30fe3a8d159f2bba70e1c20a
nugrhrizki <nugraha07rizki@gmail.com>
2021-09-12 11:45:36 +0700

    high contrast

--- config.def.h
+++ config.def.h
@@ -18,15 +18,16 @@ static const char *fonts[]          = {
   "JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
+static const char col_orange[]      = "#ff8700";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan   },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_orange },
 };
 
 /* tagging */
