35d5547121d5397895820ef0608c667d1d20a323
nugrhrizki <nugraha07rizki@gmail.com>
2022-10-16 20:05:10 +0700

    [UPDATE] nogaps

--- config.def.h
+++ config.def.h
@@ -2,10 +2,10 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
+static const Gap default_gap        = {.isgap = 0, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 1;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft  = 1;   /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 5;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
@@ -14,20 +14,19 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const char *fonts[]          = {
-  "Iosevka Nerd Font:pixelsize=10:antialias=true:autohint=true",
+  "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true",
   "JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#005577";
-static const char col_orange[]      = "#ff8700";
+static const char col_gray1[]       = "#1C1C1C";
+static const char col_gray2[]       = "#0F0F0F";
+static const char col_gray3[]       = "#DDE1E3";
+static const char col_gray4[]       = "#2D2D2D";
+static const char col_cyan[]        = "#BFDBFE";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_cyan   },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_orange },
+	[SchemeNorm] = { col_gray3, col_gray2, col_gray4 },
+	[SchemeSel]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
 /* tagging */
@@ -40,6 +39,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "explorer.exe",  NULL,  NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -77,19 +77,22 @@ static const char *dmenucmd[] = {
     "-fn", dmenufont,
     "-nb", col_gray1,
     "-nf", col_gray3,
-    "-sb", col_orange,
-    "-sf", col_gray4,
-    "-nhb", col_cyan,
-    "-nhf", col_gray3,
-    "-shb", col_orange,
-    "-shf", col_gray4,
+    "-sb", col_gray2,
+    "-sf", col_cyan,
+    "-nhb", col_gray1,
+    "-nhf", col_cyan,
+    "-shb", col_gray2,
+    "-shf", col_cyan,
     NULL
 };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+
+static const char *printcmd[]  = { "scrotc", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Print,  spawn,          {.v = printcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -144,4 +147,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
